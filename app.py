feb0401f1a4a589ce32caade0491488c6276bea6
antonver <anton.verbovyi@gmail.com>
2025-08-03 13:50:08 +0200

    Исправлена конфигурация ASGI для Render - добавлен UvicornWorker в gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """
-WSGI приложение для совместимости с Render
+ASGI приложение для совместимости с Render
 Импортирует FastAPI приложение из main.py
 """
 
 # Импортируем FastAPI приложение
 from main import app
 
-# Создаем WSGI приложение для gunicorn
+# Экспортируем ASGI приложение
 application = app 
